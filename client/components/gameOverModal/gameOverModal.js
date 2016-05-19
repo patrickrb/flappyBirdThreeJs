fe5eef4957376eeb1c1749ca6343ee0b0700a70d
Patrick Burns <pburns@kineticsupply.com>
2016-05-19 11:48:39 -0500

    better game menus

--- client/components/gameOverModal/gameOverModal.js
+++ client/components/gameOverModal/gameOverModal.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('flappyBirdThreeJs')
-    .directive('gameOverModal', function($rootScope) {
+    .directive('gameOverModal', function($rootScope, pointsService) {
         return {
             restrict: 'E',
             templateUrl: 'components/gameOverModal/gameOverModal.html',
             link: function($scope, elem) {
               elem.hide();
               $rootScope.$on('gameOver', function(){
+                $scope.points = pointsService.getPoints();
                 elem.fadeIn();
               });
 
